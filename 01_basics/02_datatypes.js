2b857e00b2202a937bb0ded7efa2e69d7936b8ca
mdali017 <mohabbatalit8@gmail.com>
2024-01-24 23:22:35 +0600

    02_datatypes.js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello"); // we are using node.js not browser
+// alert("hello");
+
+console.log(3 + 3); // code readability should be high
+console.log("hitesh");
+
+let state;
+
+let name = "hitesh";
+let age = 18;
+let isLoggedIn = false;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standdalone value
+// undifined =>
+// symbol => unique
+
+// object
+console.log(typeof "hite");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
