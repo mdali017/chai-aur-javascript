41432e1d2de1ae8c8d2b7e9e746d183031e223c0
mdali017 <mohabbatalit8@gmail.com>
2024-01-24 23:40:32 +0600

    conversation Operation

---
+++ 01_basics/03_conversationOperation.js
@@ -0,0 +1,34 @@
+// let score = 33;
+// let score = "33";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let score = "33";
+// let score = "33hjg";   // NaN
+let score = null; // 0
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber); // number
+console.log(valueInNumber); // NaN
+
+// "33" => 33
+// "33adfd" => NaN
+// true = 1; false = 0
+
+// let isLoggedIn = true;
+// let isLoggedIn = 1; // true
+// let isLoggedIn = "hitesh"; // true
+let isLoggedIn = ""; // false
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" = true
+
+let someNumber = 333;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
